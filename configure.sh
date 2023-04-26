470200e7f660a391dc6142d77e76e1bd1f6e7f6b
triet <pvmtriet232@gmail.com>
2023-04-27 00:10:47 +0700

    fix region

--- configure.sh
+++ configure.sh
@@ -2,7 +2,7 @@
 set -e
 PROFILE_NAME=tutorial
 CLUSTER_NAME=tutorial-cluster
-REGION=us-west-2
+REGION=us-east-2
 LAUNCH_TYPE=EC2
 ecs-cli configure profile --profile-name "$PROFILE_NAME" --access-key "$AWS_ACCESS_KEY_ID" --secret-key "$AWS_SECRET_ACCESS_KEY"
 ecs-cli configure --cluster "$CLUSTER_NAME" --default-launch-type "$LAUNCH_TYPE" --region "$REGION" --config-name "$PROFILE_NAME"
