5483fdde42fa333142d2463278a3f53706768a8f
triet <pvmtriet232@gmail.com>
2023-04-27 00:13:54 +0700

    change size to 1

--- create-cluster.sh
+++ create-cluster.sh
@@ -3,7 +3,7 @@ KEY_PAIR=tutorial-cluster
     ecs-cli up \
       --keypair $KEY_PAIR  \
       --capability-iam \
-      --size 2 \
+      --size 1 \
       --instance-type t3.micro \
       --tags project=tutorial-cluster,owner=raphael \
       --cluster-config tutorial \
